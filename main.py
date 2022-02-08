4e739d90b7a512907dea0b5a43d23b3fe9db8c45
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-08 12:52:51 -0700

    created valid space function  and added logic

--- main.py
+++ main.py
@@ -61,3 +61,17 @@ def convert_shape_format(shape):
         positions[i] = (pos[0] - 2, pos[1] - 4)
 
     return positions
+
+
+def valid_space(shape, grid):
+    accepted_positions = [[(j, i) for j in range(
+        10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
+    accepted_positions = [j for sub in accepted_positions for j in sub]
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in accepted_positions:
+            if pos[1] > -1:
+                return False
+
+    return True
