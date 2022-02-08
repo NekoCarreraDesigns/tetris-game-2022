c160fc03150f7cbc07354432c1a49b2f27fc1352
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-08 12:39:45 -0700

    created grid function with logic

--- main.py
+++ main.py
@@ -34,3 +34,14 @@ class Piece(object):
         self.shape = shape
         self.color = shape_colors[shape.index(shape)]
         self.rotation = 0
+
+
+def create_grid(locked_positions={}):
+    grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
+                grid[i][j] = c
+    return grid
