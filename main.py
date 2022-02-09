7f321194b44858041d92b952a8b48806067653e3
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-09 13:58:26 -0700

    created clear rows function with logic

--- main.py
+++ main.py
@@ -108,3 +108,23 @@ def draw_grid(surface, row, col):
         for j in range(col):
             pygame.draw.line(surface, (128, 128, 128),
                              (sx + j * 30, sy), (sx + j * 30, sy + play_height))
+
+
+def clear_rows(grid, locked):
+    inc = 0
+    for i in range(len(grid)-1, -1, -1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
