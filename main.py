7b0a9997eff93804387d39d65b9e703209f11db2
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-09 14:08:19 -0700

    created draw next shape function with logic

--- main.py
+++ main.py
@@ -128,3 +128,21 @@ def clear_rows(grid, locked):
             if y < ind:
                 newKey = (x, y + inc)
                 locked[newKey] = locked.pop(key)
+
+
+def draw_next_shape(shape, surface):
+    font = pygame.font.SysFont("Times", 30)
+    label = font.render('Next Piece', 1, (255, 255, 255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color,
+                                 (sx + j*30, sy + i*30, 30, 30), 0)
+
+        surface.blit(label, (sx + 10, sy - 30))
