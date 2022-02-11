24b04570b878b87cd7fef32da96e6103ee033606
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-11 15:44:41 -0700

    created high score, and high score list

--- main.py
+++ main.py
@@ -246,7 +246,25 @@ def draw_next_shape(shape, surface):
     surface.blit(label, (sx + 10, sy - 30))
 
 
-def draw_window(surface, grid, score=0):
+def update_score(nscore):
+    score = max_score()
+
+    with open('scores.txt', 'w') as f:
+        if int(score) > nscore:
+            f.write(str(score))
+        else:
+            f.write(str(nscore))
+
+
+def max_score():
+    with open('scores.txt', 'r') as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+
+    return score
+
+
+def draw_window(surface, grid, score=0, last_score=0):
     surface.fill((0, 0, 0))
 
     pygame.font.init()
@@ -264,6 +282,13 @@ def draw_window(surface, grid, score=0):
 
     surface.blit(label, (sx + 20, sy + 160))
 
+    label = font.render('High Score: ' + last_score, 1, (255, 255, 255))
+
+    sx = top_left_x + play_width - 200
+    sy = top_left_y + play_height/2 + 300
+
+    surface.blit(label, (sx + 20, sy + 160))
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             pygame.draw.rect(
@@ -278,7 +303,7 @@ def draw_window(surface, grid, score=0):
 
 def main(win):
     global grid
-
+    last_score = max_score()
     locked_positions = {}
     grid = create_grid(locked_positions)
 
@@ -354,7 +379,7 @@ def main(win):
 
             score += clear_rows(grid, locked_positions) * 10
 
-        draw_window(win, grid, score)
+        draw_window(win, grid, score, last_score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
 
@@ -363,6 +388,7 @@ def main(win):
             pygame.display.update()
             pygame.time.delay(1500)
             run = False
+            update_score(score)
 
     pygame.display.quit()
 
